2cdad846cbdd34c2249cfb8886fac30ccf7c34c9
Saliou DJALO <djalosaliou@gmail.com>
2018-12-22 14:50:07 +0400

    C'est mon premier commit sur Xcode.

--- Test Source Control/ViewController.swift
+++ Test Source Control/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("First commit !")
     }
 
+    // very simple
+    // yes papa
 
 }
 
