5127f61a71cc4e1e32883ed4634821e90bff8572
Saliou DJALO <djalosaliou@gmail.com>
2018-12-22 14:45:33 +0400

    Initial Commit

---
+++ Test Source Control/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Source Control
+//
+//  Created by Saliou DJALO on 22/12/2018.
+//  Copyright © 2018 Saliou DJALO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
